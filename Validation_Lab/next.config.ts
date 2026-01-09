aa4ae0a0b69f4b2523571eabb9d8df3438dd0a3a
Jason Wang <jasonwang@JasondeMacBook-Pro.local>
2026-01-09 18:20:25 +0800

    chore: remove embedded Validation_Lab, will add as submodule

--- Validation_Lab/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-    // Validation Lab specific config
-    // Note: Most run pages default to noindex (see VLAB-GATE-05)
-};
-
-export default nextConfig;
